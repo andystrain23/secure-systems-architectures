3dbccb569608ffdd30f68a23b735ae4cc0f59e55
Andy <ajstrain@hotmail.com>
2020-11-19 22:37:22 +0000

    incomplete skeleton added to read

--- read.py
+++ read.py
@@ -1,6 +1,18 @@
 import argparse
-# import cryptography
+from getpass import getpass
+# toremove: import cryptography
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--username', action='store')
 args = parser.parse_args()
+
+user_input = args.username
+# toremove: get password from server
+
+
+def login(username):
+    pw_input = getpass(f'Hello {username}, please input your password to continue: ')
+    print(pw_input)  # toremove: VERY DEFINITELY NOT IN FINAL CODE!!!!!!!
+
+
+login(user_input)
