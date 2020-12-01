edf1e2d7c735df11d4423eec255d71278e05045b
Andy <ajstrain@hotmail.com>
2020-12-01 11:09:40 +0000

    Server sqlite code to build database

--- server.py
+++ server.py
@@ -1,4 +1,37 @@
 import sqlite3
+import os.path
+import time
 
-connection = sqlite3.connect("database.db")
-c = connection.cursor()
+DATABASE = 'database.db'
+
+
+def build_database():
+    print('Building database...')
+    connection = sqlite3.connect(DATABASE)
+    c = connection.cursor()
+
+    c.execute('''CREATE TABLE users (
+    username TEXT UNIQUE,
+    password TEXT,
+    last_login DATE
+    )''')
+
+    c.execute('''CREATE TABLE messages (
+    date text not null,
+    sender text not null,
+    receiver text not null,
+    message text,
+    FOREIGN KEY (sender) REFERENCES users(username)
+    FOREIGN KEY (receiver) REFERENCES users(username)
+    primary key(date, sender, receiver)
+    )''')
+    connection.commit()
+    connection.close()
+    time.sleep(1)
+    print('... database built')
+
+
+if not os.path.isfile(DATABASE):
+    build_database()
+else:
+    print('Database already built')
